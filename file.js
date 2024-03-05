83dbe86384b2868f623f7fdbfbea5b8a37899f09
Marjory Washio <marjorywashio@hotmail.com>
2024-03-05 16:27:49 -0300

    Download do arquivo

--- file.js
+++ file.js
@@ -1,5 +1,15 @@
-const blob = document.querySelector(".file")
+import { saveAs } from "file-saver";
 
 function enviarArquivo(){
-    console.log('OK');
+    const fileInput = document.getElementById('file');
+    const file = fileInput.files[0];
+
+    if (file){
+        saveAs(file, "arquivo.txt");
+    } else{
+        console.error("Arquivo não selecionado");
+    }
+    
 }
+
+export default enviarArquivo;
